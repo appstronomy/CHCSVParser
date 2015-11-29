cfd3f192e332cc4ae1d5c486a32ff0ce8cef59ac
Ken Grigsby <kgrigsby@mac.com>
2015-11-28 23:32:30 -0500

    Make framework for Carthage

---
+++ CHCSVParserKit/CHCSVParserKit.h
@@ -0,0 +1,20 @@
+//
+//  CHCSVParserKit.h
+//  CHCSVParserKit
+//
+//  Created by Ken Grigsby on 11/28/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CHCSVParserKit.
+FOUNDATION_EXPORT double CHCSVParserKitVersionNumber;
+
+//! Project version string for CHCSVParserKit.
+FOUNDATION_EXPORT const unsigned char CHCSVParserKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHCSVParserKit/PublicHeader.h>
+
+
+#import <CHCSVParserKit/CHCSVParser.h>
